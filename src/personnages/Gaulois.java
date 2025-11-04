61156f1e42da9061b43adf745d07f64aeeebb363
redfoxleader <chalmettevalentin@gmail.com>
2025-11-04 17:01:26 +0100

    le village

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,7 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion;
+	private Village village;
 
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -13,12 +14,15 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
-
-	public void parler(String texte) {
-		System.out.println(predreParole() + "\"" + texte + "\"");
+	public void setVillage(Village village) {
+		this.village = village;
 	}
 
-	private String predreParole() {
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+
+	private String prendreParole() {
 
 		return "Le gaulois " + nom + " : ";
 	}
@@ -27,6 +31,17 @@ public class Gaulois {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + nomRomain);
 		romain.recevoirCoup(force*effetPotion / 3);
 		effetPotion -= 1;
+	}
+	public void sePresenter() {  
+	    if (village == null) {
+	        parler("Bonjour, je m'appelle " + nom + ". Je voyage de villages en villages.");
+	    }
+	    else if (village.getChef() == this) {
+	        parler("Bonjour, je m'appelle " + nom + ". Je suis le chef du village " + village.getNom() + ".");
+	    }
+	    else {
+	        parler("Bonjour, je m'appelle " + nom + ". J'habite le village " + village.getNom() + ".");
+	    }
 	}
 public static void main(String[] args) {
 	Gaulois asterix = new Gaulois("Astérix",8);
